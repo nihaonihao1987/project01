549bbc15c30444458e5dfa2a9c52e4943b79814a
jiangyongquan <jiangyongquan@nuctech.com>
2021-08-31 10:37:58 +0800

    提交controller

---
+++ Project/2021/外设/code/project01/mavendemo/src/main/java/com/jonny/controller/TestController.java
@@ -0,0 +1,8 @@
+/**
+ *@author JonnyJiang
+ *@create 2021-08-31 10:31
+ */
+package com.jonny.controller;
+
+public class TestController {
+}
---
+++ Project/2021/外设/code/project01/mavendemo/src/main/java/com/jonny/controller/demoController.java
@@ -0,0 +1,8 @@
+/**
+ *@author JonnyJiang
+ *@create 2021-08-31 10:32
+ */
+package com.jonny.controller;
+
+public class demoController {
+}
